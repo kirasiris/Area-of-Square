5a5d9779823dea053f20a90fc5a8fd9ed409f8db
Kevin Azuara <kevin.azuarafonseca@my.tccd.edu>
2019-11-25 04:29:03 +0000

    Finished the program by creating a setter and a getter to calculate the area of two squares.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
 
+using namespace std;
 
+class Square {
+  private:
+    double side;
+  public:
+    void setSide(double sd){ side = sd; }
+    double getSide() const { return side; }
+    double calculateArea() {
+      double result = side * side;
+      return result;
+    }
+};
 
 int main() {
-  std::cout << "Hello World!\n";
+  // First instance
+  Square S1;
+  S1.setSide(5.0);
+  cout << S1.calculateArea();
+
+  // New line
+  cout << endl;
+
+  // Second instance
+  Square S2;
+  S2.setSide(7.4);
+  cout << S2.calculateArea();
 }
